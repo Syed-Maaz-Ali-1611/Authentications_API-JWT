ef063bbae8322ff1e5850aa916e7555a31285058
SyedMaazAli-Sasquatch <maazali1611@gmail.com>
2025-12-24 04:00:47 +0500

    Setting Up things

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import authRoutes from './routes/auth.routes.js';
 const app = express();
 app.use(cors())
 app.use(express.json());
-
+//routes
 app.use("/api/auth" , authRoutes)
 
 export default app;
